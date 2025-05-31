b7613edc555dc0584e0e304156dda9628d9f3137
Leticia-Lemus <leticcia.lemux@gmail.com>
2025-05-31 09:51:16 -0600

    Update inference.py

--- inference.py
+++ inference.py
@@ -3,11 +3,14 @@ import numpy as np
 
 # Modelo entrenado
 model = joblib.load("avocado_model.pkl")
+scaler = joblib.load("scaler.pkl")
 
 # Ejemplo de datos nuevos con o caracterpisticas
 X_nuevo = np.array([[0.55, 120.3, 0.78, 0.65, 2, 58.9, 170.2, 145.6]])
+#Aplicar escalamiento
+X_nuevo_escalado = scaler.transform(X_nuevo)
 
 # Predecir nivel de maduración
-prediccion = model.predict(X_nuevo)
+prediccion = model.predict(X_nuevo_escalado)
 
 print("Predicción del nivel de maduración:", prediccion)
